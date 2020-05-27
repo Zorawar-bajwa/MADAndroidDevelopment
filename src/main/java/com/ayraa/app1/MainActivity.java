4fcc73d7f296a3b1ffc1018fc96503465f8f0b20
Zorawar-bajwa <zorawarbajwa7@gmail.com>
2020-05-27 18:46:27 -0400

    Discontinuing some activities
    For educational purpose

--- src/main/java/com/ayraa/app1/MainActivity.java
+++ src/main/java/com/ayraa/app1/MainActivity.java
@@ -44,12 +44,7 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void aquarius(View view) {
-        Toast.makeText(getApplicationContext(), "Aquarius", Toast.LENGTH_SHORT).show();
-        Intent intent = new Intent(this, Details.class);
-        intent.putExtra("sign", "aquarius".toString());
-        startActivity(intent);
-    }
+    
 
     public void virgo(View view) {
         Toast.makeText(getApplicationContext(), "Virgo", Toast.LENGTH_SHORT).show();
